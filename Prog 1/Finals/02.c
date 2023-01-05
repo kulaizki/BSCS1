fc0d24fa66eda5ad6215b91fc1e4a5baa4c5218a
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-05 16:41:12 +0800

    Update 02.c

--- Prog 1/Finals/02.c
+++ Prog 1/Finals/02.c
@@ -9,23 +9,18 @@ void Main() {
     printf("Mister Connoisseur, please rate my fruit.\n");
     printf("My my, your fruit is : ");
     scanf("%f", &connoisseur);
-    printf("FRUWI says... %c", convert2Letter(connoisseur));
-    
+    printf("FRUWI says... %c", convert2Letter(connoisseur));  
 }
 
 char convert2Letter (float rating) {
 
     char grade[] = "SABC"; // do not change this line of code
-
     if (rating == 10.0)
         return grade[0];
-
     if (rating < 10.0 && rating >= 8.0)
         return grade[1];
-
     if (rating < 8 && rating >= 7)
         return grade[2];
-
     if (rating < 7)
         return grade[3];    
 }
