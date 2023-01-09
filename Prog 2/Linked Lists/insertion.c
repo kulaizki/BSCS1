22dd04dc430a7331e87852d0acc0159f36cef9aa
kulaizki <kulaizke@gmail.com>
2023-01-09 15:22:44 +0800

    insertionList

--- Prog 2/Linked Lists/insertion.c
+++ Prog 2/Linked Lists/insertion.c
@@ -3,6 +3,7 @@
 
 void insertFront(struct Node* head, int newValue);
 void insertEnd(struct Node* head, int newValue);
+void insertAtPos(struct Node* head, int newValue, int pos);
 
 struct Node {
     int value;
@@ -31,6 +32,44 @@ void insertFront(struct Node* head, int newValue) {
 }
 
 void insertEnd(struct Node* head, int newValue) {
+    // create the new node
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->value = newValue;
 
+    // we create a pointer to the head
+    struct Node* current = head;
+
+    // we traverse through the list until we reach
+    // the last element of the list
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    // we make the last node point to the new node
+    current->next = newNode;
+}
+
+void insertAtPos(struct Node* head, int newValue, int pos) {
+    // create the new node
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->value = newValue;
+
+    // we create a pointer to the head
+    struct Node* current = head;
+
+    // we go to the position where we will insert
+    // the new node
+    int i = 0;
+    while (i < pos && current != NULL) {
+        current = current->next;
+        i++;
+    }
+
+    // we insert the new node
+    if (current != NULL) {
+        struct Node* temp = current-> next;
+        current->next = newNode;
+        newNode->next = temp;
+    }
 }
 
