032266526d851599df76f30658127b374500b8f2
kulaizki <kulaizke@gmail.com>
2023-01-10 16:56:18 +0800

    deletionFree

--- Prog 2/Linked Lists/deletion.c
+++ Prog 2/Linked Lists/deletion.c
@@ -6,13 +6,73 @@ struct Node {
     struct Node* next;
 };
 
+void freeList(struct Node* head);
+void display(struct Node* head);
 void deleteFront(struct Node* head);
 void deleteEnd(struct Node* head);
 void deleteAtPos(struct Node* head, int pos);
 
 int main()
 {
+    struct Node* head = (struct Node*) malloc(sizeof(struct Node));
+
+    struct Node* node1 = (struct Node*) malloc(sizeof(struct Node));
+    node1-> value = 7;
+    head->next = node1;
+
+    struct Node* node2 = (struct Node*) malloc(sizeof(struct Node));
+    node2->value = 14;
+    node1->next = node2;
+
+    struct Node* node3 = (struct Node*) malloc(sizeof(struct Node));
+    node3->value = 21;
+    node2->next = node3;
+
+    struct Node* node4 = (struct Node*) malloc(sizeof(struct Node));
+    node4->value = 28;
+    node3->next = node4;
+
+    struct Node* node5 = (struct Node*) malloc(sizeof(struct Node));
+    node5->value = 35;
+    node4->next = node5;
     
+    node5->next = NULL;
+
+    printf("Initial list: ");
+    display(head);
+
+    printf("\ndeleteEront: ");
+    deleteFront(head);
+    display(head);
+
+    printf("\ndeleteEnd: ");
+    deleteEnd(head);
+    display(head);
+
+    printf("\ndeleteAtPos: ");
+    deleteAtPos(head, 1);
+    display(head);
+
+    freeList(head);
+
+}
+
+void display(struct Node* head) {
+    struct Node* current = head->next;
+
+    while (current != NULL) {
+        printf("%d ", current->value);
+        current = current->next;
+    }
+}
+
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
 }
 
 void deleteFront(struct Node* head) {
