733d902145bc17668c45f5bfd2e90f185ec421a9
kulaizki <kulaizke@gmail.com>
2023-01-15 19:10:11 +0800

    insert

--- Prog 2/Linked Lists/TISD-linked-list.c
+++ Prog 2/Linked Lists/TISD-linked-list.c
@@ -24,19 +24,55 @@ int main()
 }
 
 void display(struct Node* head) {
-
+    struct Node* current = head->next;
+    while (current != NULL) {
+        printf("%d ", current->value);
+        current = current->next;
+    }
 }
 
 void insertFront(struct Node* head, int val) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->value = val;
 
+    struct Node* firstNode = head->next;
+
+    head->next = newNode;
+
+    newNode->next = firstNode;
 }
 
 void insertEnd(struct Node* head, int val) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->value = val;
+    newNode->next = NULL;
 
+    struct Node* current = head;
+
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
 }
 
-void insertAtPos(struct Node*head, int val, int pos) {
+void insertAtPos(struct Node* head, int val, int pos) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->value = val;
 
+    struct Node* current = head;
+
+    int i = 0;
+    while (i < pos && current != NULL) {
+        current = current->next;
+        i++;
+    }
+
+    if (current != NULL) {
+        struct Node* temp = current-> next;
+        current->next = newNode;
+        newNode->next = temp;
+    }
 }
 
 bool search(struct Node* head, int val) {
