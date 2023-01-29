8109a0643344e39a92249ccb235003858f00c432
kulaizki <kulaizke@gmail.com>
2023-01-29 21:22:01 +0800

    sorting

--- Prog 2/Linked Lists/doubly-list_train.c
+++ Prog 2/Linked Lists/doubly-list_train.c
@@ -18,7 +18,6 @@ struct Node* createNode(int value) {
 
 void insertAtHead(int value) {
     struct Node* newNode = createNode(value);
-
     if (head == NULL) {
         head = newNode;
         return;
@@ -31,7 +30,6 @@ void insertAtHead(int value) {
 
 void insertAtTail(int value) {
     struct Node* newNode = createNode(value);
-
     if (head == NULL) {
         head = newNode;
         return;
@@ -44,6 +42,29 @@ void insertAtTail(int value) {
 
     current->next = newNode;
     newNode->prev = current;
+}
+
+void insertAtPos(int value, int position) {
+    struct Node* newNode = createNode(value);
+    struct Node* current = head;
+    int index = 0;
+
+    if (position == 0) {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        return;
+    }
+
+    while (index < position-1 && current != NULL) {
+        current = current->next;
+        index++;
+    }
+
+    newNode->next = current->next;
+    newNode->prev = current;
+    if (head != NULL) { current->next->prev = newNode; }
+    current->next = newNode;
 }
 
 void sortListAscending() {
@@ -69,11 +90,33 @@ void sortListAscending() {
         }
         node1 = node1->next;
     }
-    printf("\nSorted\n");
+    printf("\nSorted in Ascending Order\n");
 }
 
 void sortListDescending() {
+    if (head == NULL || head->next == NULL) {
+        printf("List does not have enough nodes to sort\n");
+        return;
+    }
 
+    struct Node* node1;
+    struct Node* node2;
+    int temp;
+    node1 = head;
+
+    while (node1->next != NULL) {
+        node2 = node1->next;
+        while (node2 != NULL) {
+            if (node1->data < node2->data) {
+                temp = node1->data;
+                node1->data = node2->data;
+                node2->data = temp;
+            }
+            node2 = node2->next;
+        }
+        node1 = node1->next;
+    }
+    printf("\nSorted in Descending Order\n");
 }
 
 void displayHeadToTail() {
@@ -121,13 +164,24 @@ void freeList(struct Node* head) {
 int main()
 {
     insertAtHead(4);
+    insertAtHead(7);
+    insertAtHead(1);
+    insertAtHead(6);
+    insertAtHead(9);
+    printf("Current list\n"); displayHeadToTail();
 
-    displayHeadToTail(); displayTailToHead();
-
+    insertAtPos(2, 3);
+    printf("\nInserted 2 in position 3\n"); displayHeadToTail();
+    insertAtPos(0, 0);
+    printf("\nInserted 0 in position 0\n"); displayHeadToTail();
+    displayTailToHead();
     
     sortListAscending(); 
     displayHeadToTail(); displayTailToHead();
-    
+
+    sortListDescending();
+    displayHeadToTail(); displayTailToHead();
+
     freeList(head);
 
     return 0;
