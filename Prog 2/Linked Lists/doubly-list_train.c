8e635c9ef75ec187461005e4354d64dc2aa9e8e9
kulaizki <kulaizke@gmail.com>
2023-01-29 19:22:56 +0800

    sortAscending

--- Prog 2/Linked Lists/doubly-list_train.c
+++ Prog 2/Linked Lists/doubly-list_train.c
@@ -46,10 +46,28 @@ void insertAtTail(int value) {
     newNode->prev = current;
 }
 
-void sortList() {
-    if (head == NULL) {
-        printf("List is empty\n");
+void sortListAscending() {
+    if (head == NULL || head->next == NULL) {
+        printf("List does not have enough nodes to sort\n");
         return;
+    }
+
+    struct Node* node1;
+    struct Node* node2;
+    int temp;
+    node1 = head;
+
+    while (node1->next != NULL) {
+        node2 = node1->next;
+        while (node2 != NULL) {
+            if (node1->data > node2->data) {
+                temp = node1->data;
+                node1->data = node2->data;
+                node2->data = temp;
+            }
+            node2 = node2->next;
+        }
+        node1 = node1->next;
     }
 }
 
@@ -74,7 +92,7 @@ void displayTailToHead() {
         printf("List is empty\n");
         return;
     }
-    
+
     struct Node* current = head;
 
     while (current->next != NULL) { current = current->next; }
@@ -97,13 +115,16 @@ void freeList(struct Node* head) {
 
 int main()
 {
-    insertAtHead(10);
-    insertAtHead(20);
-    insertAtTail(30);
-    insertAtTail(40);
+    insertAtHead(4);
+    insertAtHead(9);
+    insertAtTail(1);
+    insertAtTail(3);
+    displayHeadToTail(); displayTailToHead();
 
-    displayHeadToTail();
-    displayTailToHead();
+    printf("\nSorted\n");
+    sortListAscending(); 
+    displayHeadToTail(); displayTailToHead();
+    
     
     freeList(head);
 
