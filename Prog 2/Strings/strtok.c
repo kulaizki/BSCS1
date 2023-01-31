5c98c2f20f6886d967be345f1df2cb3b96b2d67a
kulaizki <kulaizke@gmail.com>
2023-01-31 15:27:20 +0800

    strtok

--- Prog 2/Strings/strtok.c
+++ Prog 2/Strings/strtok.c
@@ -3,5 +3,17 @@
 
 int main()
 {
+    // strtok syntax
+    // char *strtok(char *str, const char *delim);
+
+    char str[] = "This is a sample string";
+    const char s[2] = " ";
+
+    char *token;
+    token = strtok(str, s);
     
+    while(token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, s);
+    }
 }
