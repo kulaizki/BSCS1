ba5b345efb54c1672e5592fd7b86e6be3751d141
kulaizki <kulaizke@gmail.com>
2023-02-07 20:20:53 +0800

    format

--- Prog 2/Sorting/insertion-sort.c
+++ Prog 2/Sorting/insertion-sort.c
@@ -2,14 +2,14 @@
 
 void insertionSort(int arr[], int n) {
     int x, key, y;
-    for (x = 1; x < n; x++) {
+    for (x = 1; x < n; ++x) {
         key = arr[x];
-        y = x - 1;
+        y = x-1;
         while (y >= 0 && arr[y] > key) {
             arr[y + 1] = arr[y];
-            y = y - 1;
+            y = y-1;
         }
-        arr[y + 1] = key;
+        arr[y+1] = key;
     }
 }
 
