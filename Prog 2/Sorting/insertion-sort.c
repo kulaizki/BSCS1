a5248ea5a464f6d283b47029e7921795d70cdb8d
kulaizki <kulaizke@gmail.com>
2023-02-07 19:53:36 +0800

    selectionSort

---
+++ Prog 2/Sorting/insertion-sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+
+}
+
+void display(int arr[], int n) {
+    int x;
+    printf("Current array: ");
+    for (x = 0; x < n; ++x) {
+        printf("%d ", arr[x]);
+    }
+    printf("\n");
+}
---
+++ Prog 2/Sorting/selection-sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    int x, y, min_index;
+    for (x = 0; x < n-1; ++x) {
+        min_index = x;
+        for (y = x+1; y < n; ++y) {
+            if (arr[y] < arr[min_index])
+                min_index = y;
+        }
+        swap(&arr[x], &arr[min_index]);
+    }
+}
+
+void display(int arr[], int n) {
+    int x;
+    printf("Current array: ");
+    for (x = 0; x < n; ++x) {
+        printf("%d ", arr[x]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {4, 9, 2, 1};
+
+    display(arr, 4);
+    selectionSort(arr, 4);
+    display(arr, 4);
+}
