ce2f7f917e49e2eb0f6d0ded06bd42b1f556bba0
kulaizki <kulaizke@gmail.com>
2023-02-07 19:56:53 +0800

    insertionSort

--- Prog 2/Sorting/insertion-sort.c
+++ Prog 2/Sorting/insertion-sort.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 void insertionSort(int arr[], int n) {
-
+    int x, key, y;
+    for (x = 1; x < n; x++) {
+        key = arr[x];
+        y = x - 1;
+        while (y >= 0 && arr[y] > key) {
+            arr[y + 1] = arr[y];
+            y = y - 1;
+        }
+        arr[y + 1] = key;
+    }
 }
 
 void display(int arr[], int n) {
@@ -11,4 +20,13 @@ void display(int arr[], int n) {
         printf("%d ", arr[x]);
     }
     printf("\n");
+}
+
+int main()
+{
+    int arr[] = {4, 9, 2, 1, 5};
+
+    display(arr, 5);
+    insertionSort(arr, 5);
+    display(arr, 5);
 }
