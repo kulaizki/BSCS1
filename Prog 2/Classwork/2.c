5a373a785faa0ca79a128b58b311d777e2e5c239
kulaizki <kulaizke@gmail.com>
2023-02-24 23:46:16 +0800

    format

--- Prog 2/Classwork/2.c
+++ Prog 2/Classwork/2.c
@@ -2,28 +2,24 @@
 #include <stdlib.h>
 
 int *createArray() {
+
     int n, i;
     scanf("%d", &n);
 
     int *newArr = (int *)malloc(sizeof(int) * n + 1);
     newArr[0] = n;
 
-    for (i = 1; i <= n; ++i) 
-    {
-        scanf("%d", &newArr[i]);
-    }
+    for (i = 1; i <= n; ++i) scanf("%d", &newArr[i]);
 
     return newArr;
 }
 
-int main()
-{
+int main() {
+
     int *arr;
     arr = createArray();
 
-    for (int i = 1; i <= arr[0]; i++) {
-        printf("%d ", arr[i]);
-    }
+    for (int i = 1; i <= arr[0]; i++) printf("%d ", arr[i]);
 
     free(arr);
 }
