2cf0d3bf7eda2b56502a6b19122cf37f72f3f558
kulaizki <kulaizke@gmail.com>
2023-02-24 23:48:27 +0800

    format

--- Prog 2/Classwork/7_StrBubbleSort.c
+++ Prog 2/Classwork/7_StrBubbleSort.c
@@ -2,26 +2,27 @@
 #include <string.h>
 
 void swap(char *x, char *y) {
+    
     char temp = *x;
     *x = *y;
     *y = temp;
 }
 
 void bubbleSort(char *str) {
-    int x, y, count = strlen(str)-1;
 
-    for (x = 1; x <= count; ++x) {
-        for (y = count; y >= x; --y) {
-            if (str[y] < str[y-1]) {
-                swap(str+y, str+y-1);
-            }
+    int x, y, count = strlen(str)-1;
+    for (x = 1; x <= count; ++x) 
+    {
+        for (y = count; y >= x; --y) 
+        {
+            if (str[y] < str[y-1]) swap(str+y, str+y-1);
             puts(str);
         }
     }
 }
 
-int main()
-{
+int main() {
+
     char str[9] = "zxjchsad";
     bubbleSort(str);
 }
