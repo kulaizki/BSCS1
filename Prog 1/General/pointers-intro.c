17a07e0c19edad907a09ab85db87cbf78e13e06c
kulaizki <kulaizke@gmail.com>
2023-02-25 23:34:12 +0800

    format

---
+++ Prog 1/General/pointers-intro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x = 5, y = 10;
+
+    int *p1;
+    p1 = &x;
+    int *p2 = &y;
+
+    // p = &x address of x
+    // p2 = &y address of y
+    // *p = x
+    // *p2 = y
+
+    // dereferencing
+    printf("x = %d\n", x); // 5
+    printf("y = %d\n", y); // 10
+    printf("&x = %d\n", &x); // address of x
+    printf("&y = %d\n", &y); // address of y
+    printf("p1 = %d\n", p1); // address of x
+    printf("y = %d\n", p2); // address of y
+    printf("*p1 = %d\n", *p1); // 5
+    printf("*p2 = %d\n", *p2); // 10
+}
---
+++ Prog 1/General/swap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 5, y = 9, temp;
+    
+    temp = x;
+    x = y;
+    y = temp;
+    
+}
