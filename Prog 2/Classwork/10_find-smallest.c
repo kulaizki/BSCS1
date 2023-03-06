61b81bc7f1449a2ef6226f63ed427a5d956e80d7
kulaizki <kulaizke@gmail.com>
2023-03-06 09:55:49 +0800

    min elem

--- Prog 2/Classwork/10_find-smallest.c
+++ Prog 2/Classwork/10_find-smallest.c
@@ -17,18 +17,8 @@ void findSmallest(char *s) {
     }
 }
 
-void display(int arr[], int n) {
-
-    int x;
-    printf("Current array: ");
-    for (x = 0; x < n; ++x) {
-        printf("%d ", arr[x]);  
-    }
-    printf("\n");
-}
-
-int main() 
-{
+int main() {
+    
     char s[] = "hella";
     findSmallest(s);
     puts(s);
