35b9047192d762eb62eaa2ea4d98b545f64dc8d3
kulaizki <kulaizke@gmail.com>
2023-03-10 11:37:06 +0800

    unsigned long long main >:D

--- Prog 2/General/test.c
+++ Prog 2/General/test.c
@@ -1,43 +1,8 @@
 #include <stdio.h>
 
-typedef struct student
-{
-    char fname[24];
-    char MI;
-    char lname[16];
-    int age;
-    char gender;
-} Student;
+unsigned long long main() {
 
-void input(Student *name);
-void display(Student *name);
+    printf("Hello, world!");
 
-int main()
-{
-    Student Fitz;
-    input(&Fitz);
-    display(&Fitz);
-}
-
-void input(Student *name)
-{
-    printf("enter fname: ");
-    gets(name->fname);
-    printf("enter MI:");:e
-    name->MI = getchar();
-    printf("enter lname: ");
-    fflush(stdin);
-    gets(name->lname);
-    printf("enter age: ");
-    scanf("%d", &name->age);
-    fflush(stdin);
-    printf("enter gender: ");
-    name->gender = getchar();
-}
-
-void display(Student *name)
-{
-    printf("full name: %s %c. %s", name->fname, name->MI, name->lname);
-    printf("\nage: %d", name->age);
-    printf("\ngender: %c", name->gender);
+    return 0;
 }
