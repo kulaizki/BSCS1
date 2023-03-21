2c62a83002d0ff23f3f831fbfe57c731323b29c3
kulaizki <kulaizke@gmail.com>
2023-03-21 15:05:10 +0800

    iterative l <= r

---
+++ Prog 2/General/binary-search-iterative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void binarySearch(int arr[], int n, int x) {
+
+    int l = 0, h = n-1;
+
+    while (l <= h) {
+        int m = l + (h - l) / 2;
+        if (x == arr[m]) {
+            printf("%d is in index %d\n", x, m);
+            return;
+        }
+        
+        if (x > arr[m]) {
+            l = m + 1;
+        } else {
+            h = m - 1;
+        }
+    }
+
+    printf("%d NOT FOUND", x);
+}
+
+int main() {
+
+    int arr[] = {1, 3, 6, 8, 12, 13, 55, 99};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    binarySearch(arr, n, 1);
+}
