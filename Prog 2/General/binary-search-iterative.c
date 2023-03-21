2c94ed5ded304828699bbc6acf20f9e3e01877ec
kulaizki <kulaizke@gmail.com>
2023-03-21 15:22:50 +0800

    l <= h

--- Prog 2/General/binary-search-iterative.c
+++ Prog 2/General/binary-search-iterative.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
-int binarySearch(int arr[], int l, int h, int x) {
+void binarySearch(int arr[], int n, int x) {
 
-    if (l > h) {
-        return -1; 
+    int l = 0, h = n - 1;
+
+    while (l <= h) {
+        int m = l + (h - l) / 2;
+
+        if (x == arr[m]) {
+            printf("%d found at index %d\n", x, m);
+            return;
+        } 
+
+        if (x > arr[m]) {
+            l = m + 1;
+        } else {
+            h = m - 1;
+        }
     }
 
-    int m = l + (h - l) / 2;
-
-    if (arr[m] == x) {
-        return m;
-    } else if (x > arr[m]) {
-        return binarySearch(arr, m + 1, h, x);
-    } else {
-        return binarySearch(arr, l, m - 1, x);
-    }
+    printf("%d not found\n", x);
 }
 
 int main() {
 
     int arr[] = {1, 3, 6, 8, 12, 13, 55, 99};
-    int l = 0, h = sizeof(arr) / sizeof(arr[0]);
-    int index = binarySearch(arr, l, h, 55);
-
-    (index == -1) ? printf("Element not found\n")
-                  : printf("Element found at index %d", index);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    binarySearch(arr, n, 55);
 }
