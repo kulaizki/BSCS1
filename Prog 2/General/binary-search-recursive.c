04d1ee57748bdcd95e8607d4459de11aa3ac8ff4
kulaizki <kulaizke@gmail.com>
2023-03-21 15:19:21 +0800

    (arr, l, h, 55)

--- Prog 2/General/binary-search-recursive.c
+++ Prog 2/General/binary-search-recursive.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-void binarySearch() {
+int binarySearch(int arr[], int l, int h, int x) {
 
+    if (l > h) {
+        return -1; 
+    }
+
+    int m = l + (h - l) / 2;
+
+    if (arr[m] == x) {
+        return m;
+    } else if (x > arr[m]) {
+        return binarySearch(arr, m + 1, h, x);
+    } else {
+        return binarySearch(arr, l, m - 1, x);
+    }
 }
 
 int main() {
 
     int arr[] = {1, 3, 6, 8, 12, 13, 55, 99};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    binarySearch(arr, n, 1);  
+    int l = 0, h = sizeof(arr) / sizeof(arr[0]);
+    int index = binarySearch(arr, l, h, 55);
+
+    (index == -1) ? printf("Element not found\n")
+                  : printf("Element found at index %d", index);
 }
