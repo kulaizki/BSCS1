6ae3630ef19f2a0e12b1102ad9b98b713fbffcf9
kulaizki <kulaizke@gmail.com>
2023-04-08 16:03:54 +0800

    strcmp number

--- Prog 2/General/test.c
+++ Prog 2/General/test.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-unsigned long long main() {
+int main() {
 
-    printf("H3ll0, w0rld!");
+    char str1[10] = "2";
+    char str2[10] = "1";
 
-    return 9999999999999999999;
+    printf("%d", strcmp(str1, str2));
+
 }
