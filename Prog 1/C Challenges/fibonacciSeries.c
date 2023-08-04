0f6589ced0e786477d80ec73851198f0af1a6b5f
kulaizki <kulaizke@gmail.com>
2023-08-04 22:14:42 +0800

    format

--- Prog 1/C Challenges/fibonacciSeries.c
+++ Prog 1/C Challenges/fibonacciSeries.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
 
     int n, num1 = 0, num2 = 1, sum;
-
+    
     printf("Enter n: ");
     scanf("%d", &n);
 
     printf("Fibonacci Sequence: ");
-
     for(int i = 0; i < n; i++) {
         printf("%d ", num1);
-        sum = num1+num2;
+        sum = num1 + num2;
         num1 = num2;
         num2 = sum;
     }
